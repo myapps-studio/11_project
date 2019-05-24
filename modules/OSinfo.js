fa8afe504d6a0b05bb7d7b34aa359935412fa271
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-05-24 21:31:21 +0200

    finished

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,5 +1,6 @@
 var os = require('os');
-var secondConv = require('./modules/timeFunc');
+var colors = require('colors');
+var secondConv = require('../modules/timeFunc'); 
 
 function getOSinfo() {
     var type = os.type();
@@ -10,13 +11,13 @@ function getOSinfo() {
     }
     var release = os.release();
     var cpu = os.cpus()[0].model;
-    var uptime = secondConv.print();
+    var uptime = secondConv.print(); 
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', uptime); 
-    console.log('User name:', userInfo.username);
+    console.log(colors.grey('System:'), type);
+    console.log(colors.red('Release:'), release);
+    console.log(colors.blue('CPU model:'), cpu);
+    console.log(colors.green('Uptime: ~'), uptime); 
+    console.log(colors.yellow('User name:'), userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
 
